c933de8d08e7b38ef6afb1f9a28b39ba740f4c32
Слава Сызоненко <jason.172583@gmail.com>
2021-03-04 17:26:40 +0200

    1

---
+++ vector/vector.cpp
@@ -0,0 +1,313 @@
+﻿// vector.cpp 
+#include <iostream>
+
+
+template<class T>
+class Vector {
+public:
+    
+  
+    Vector(){ 
+        pointer = new T[this->capacity];
+    }
+    template<class T1>
+    Vector(std::initializer_list<T1> value) : capacity{value.size()} {
+    
+    if (value.size() == 0) {
+        return;
+    }
+    else{ 
+        pointer = new T[this->capacity];
+        for (const T& ob : value) {
+            pointer[size] = ob;
+            ++size;
+             }
+        }
+    }
+ 
+    Vector(size_t size_of_vector) {
+        capacity = size_of_vector;
+        pointer = new T[this->capacity];
+    }
+    Vector(const Vector& copy) {
+        pointer = new T[copy.capacity];
+            for (decltype(size) i = 0; i < size; ++i) 
+                {
+                    this->pointer[i] = copy.pointer[i];
+                }
+            }
+        Vector& operator=(const Vector& copy) {
+            
+            pointer = new T[copy.capacity];
+            for (decltype(size) i = 0; i < size; ++i)
+            {
+                this->pointer[i] = copy.pointer[i];
+            }
+        }
+
+       
+
+
+        ~Vector() {
+            delete[] pointer;
+        }
+
+
+
+        void push_back(T value) {
+            if (capacity == 0)
+                ++capacity;
+             if(capacity > size ) {
+                pointer[size] = value;
+                ++size;
+            }
+            else {
+                this->capacity *= 2;
+                T* new_memory_vector = new T[this->capacity];
+                for (decltype(size) i = 0; i < size; ++i) {
+                    new_memory_vector[i] = pointer[i];
+                }
+                delete[] pointer;
+                pointer = new_memory_vector;
+                pointer[size] = value;
+                ++size;
+            }
+        }
+    
+        T front() {
+            if (size >= 0)
+                return pointer[0];
+            else 
+                std::cout << "error";
+        }
+
+        T back() {
+            if (size >= 0)
+                return pointer[size-1];
+            else 
+                std::cout << "error";
+
+        }
+        T* begin() {
+            return pointer;
+        }
+        T* end(){
+            return (pointer+size-1);
+        }
+
+        void write() {
+            std::cout << "vector =";
+            for (decltype(size) i = 0; i < size; ++i) {
+                std::cout << "  " << pointer[i] ;
+            }
+            std::cout << std::endl;
+        }
+
+
+        template< class Args >
+        void emplace_back(Args&& value) {
+            if (capacity > size) {
+               new (pointer+size)  T(value);
+                ++size;
+            }
+            else {
+                this->capacity *= 2;
+                T* new_memory_vector = new T[this->capacity];
+                for (decltype(size) i = 0; i < size; ++i) {
+                    new_memory_vector[i] = pointer[i];
+                }
+                delete[] pointer;
+                pointer = new_memory_vector;
+                new (pointer + size)  T(value);
+                ++size;
+            }
+            
+        }
+
+        template< class... Args, class T1 >
+        void emplace_back(T1 value, Args&& ...args) {
+            if (capacity > size) {
+                new (pointer + size) T(value);
+                ++size;
+                emplace_back(args...);
+            }
+            else {
+                this->capacity *= 2;
+                T* new_memory_vector = new T[this->capacity];
+                for (decltype(size) i = 0; i < size; ++i) {
+                    new_memory_vector[i] = pointer[i];
+                }
+                delete[] pointer;
+                pointer = new_memory_vector;
+                new (pointer + size)  T(value);
+                ++size;
+                emplace_back(args...);
+            }
+          
+        }
+        size_t size_() {
+            return size;
+        }
+        size_t capacity_() {
+            return capacity;
+        }
+        bool empty_() {
+            if (size == 0)
+                return true;
+            else
+                return false;
+        }
+
+        T* insert( T* it, T value) {
+            if (size + 1 > capacity) {
+                this->capacity *= 2;
+                T* new_memory_vector = new T[this->capacity];
+                for (decltype(size) i = 0; i < size; ++i) {
+                    new_memory_vector[i] = pointer[i];
+                }
+                delete[] pointer;
+                pointer = new_memory_vector;
+            }
+            T switch_;
+            ++size;
+            for (decltype(size) i = 0, k = 0; i < size; ++i) {
+                if (pointer + i == it) {
+                    switch_ = pointer[i];
+                    pointer[i] = value;
+                    ++i;
+                    value = pointer[i];
+                    for (; i < size; ++i) {
+                        pointer[i] = switch_;
+                        switch_= value;
+                        value = pointer[i + 1];
+                    }
+                }
+                   
+            }
+            return it;
+        }
+
+        T* erase(size_t pos) {
+
+            T* it=pointer;
+
+            if (pos > size)
+                return it;
+            --pos;
+            for (decltype(size) i = 0; i < size; ++i) {
+                if (i == pos) {
+                    it = &pointer[i];
+                    for (; i < size && (i - 1) != size; ++i)
+                        pointer[i] = pointer[i + 1];
+                }
+            }
+            --size;
+            return it;
+           
+        }
+        T* erase(T* pos) {
+
+            T* it = pointer;
+
+            if (pos )
+                return it;
+            for (decltype(size) i = 0; i < size; ++i) {
+                if (pos == (pointer + i)) {
+                    it = &pointer[i];
+                    for (; i < size && (i - 1) != size; ++i)
+                        pointer[i] = pointer[i + 1];
+                }
+            }
+            --size;
+            return it;
+
+        }
+        
+        T* erase(T* begin, T* end) {
+            T* it = begin;
+            int nsize = 0;
+            if (begin < end) {
+                nsize =  end - begin;
+            }
+            else
+                return begin;
+            
+            std::cout << "size  " << nsize << "  "  << std::endl;
+         
+            for (decltype(size) i = 0; i < size; ++i) {
+                std::cout << begin<<"  /// "<< &pointer[i]<<"   ||"<<std::endl;
+                if (begin == (pointer + i)) {
+                    for (;  end < &pointer[size]; ++i) {
+                        pointer[i] = *end;
+                        std::cout << "()()"<<pointer[i]<<"  "<<i << std::endl;
+                     
+                        end++;
+                    }
+                }
+            }
+
+            
+            size -= nsize;
+            return begin;
+        }
+
+        void pop_back() {
+            --size;
+        }
+        const T& operator[](size_t pos) const;
+        T& operator[](size_t pos) {
+            if (pos <= size) {
+                return *(pointer + pos);
+            }
+
+            else
+                return *(pointer);//exception
+       }
+
+        void resize(size_t count) {
+
+        }
+        void reserve(size_t new_cap) {
+            if (new_cap <= capacity) {
+                return;
+            }
+            T* new_memory_vector = new T[new_cap];
+            capacity = new_cap;
+            for (decltype(size) i = 0; i < size; ++i) {
+                new_memory_vector[i] = pointer[i];
+            }
+            delete[] pointer;
+            pointer = new_memory_vector;
+        }
+
+
+
+private:
+    size_t size{ 0 };
+    size_t capacity{ 0 };
+    T* pointer ;
+
+};
+
+
+//ДЛЯ проверки роботы
+class debug {
+public:
+    debug() { std::cout << "debug"<<std::endl; }
+    ~debug() { std::cout << "~debug " << std::endl; }
+    debug(int value) { std::cout << "debug int" << std::endl; }
+    debug(const debug&) { std::cout << "debug const debug&" << std::endl; }
+    debug(debug&&)noexcept { std::cout << "debug const debug&&" << std::endl; }
+    debug& operator =(const debug&) { std::cout << " == const debug&" << std::endl; return *this; }
+    debug& operator =(debug&&)noexcept { std::cout << " ==const debug&&" << std::endl; return *this; }
+};
+
+int main()
+{
+
+
+
+   
+    
+    
+}
